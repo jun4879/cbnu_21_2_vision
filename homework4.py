8c7998609ea91f5c7fd6ccd1560f87af3acfacbb
KIM JUN TAE <57665924+jun4879@users.noreply.github.com>
2021-10-13 14:23:51 +0900

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -5,37 +5,52 @@ import matplotlib.pyplot as plt
 img = cv2.imread('data/image_Peppers512rgb.png', cv2.IMREAD_GRAYSCALE)
 _, binary = cv2.threshold(img, -1, 1, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-# eroded_iteration = int(input('eroded_iteration: '))
-eroded = cv2.morphologyEx(binary, cv2.MORPH_ERODE, (3,3), iterations=10)
-dilated = cv2.morphologyEx(binary, cv2.MORPH_DILATE, (3,3), iterations=10)
+cv2.namedWindow('Peppers')
+finish = False
+while not finish:
+    cv2.imshow("Peppers", img)
+    key = cv2.waitKey(1)
 
-opened = cv2.morphologyEx(binary, cv2.MORPH_OPEN,
-                          cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)),
-                          iterations=5)
-closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE,
-                          cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)),
-                          iterations=5)
+    if key == ord('e'):
+        eroded_iteration = int(input('eroded_iteration: '))
+        eroded = cv2.morphologyEx(binary, cv2.MORPH_ERODE, (3, 3), iterations=eroded_iteration)
+        #cv2.imshow('eroded / iteration : {0}'.format(eroded_iteration), eroded)
+        plt.imshow(eroded, cmap='gray')
+        plt.show()
 
-plt.figure(figsize=(10,10))
-plt.subplot(231)
-plt.axis('off')
-plt.title('binary')
-plt.imshow(binary, cmap='gray')
-plt.subplot(232)
-plt.axis('off')
-plt.title('erode 10 times')
-plt.imshow(eroded, cmap='gray')
-plt.subplot(233)
-plt.axis('off')
-plt.title('dilate 10 times')
-plt.imshow(dilated, cmap='gray')
-plt.subplot(234)
-plt.axis('off')
-plt.title('open 5 times')
-plt.imshow(opened, cmap='gray')
-plt.subplot(235)
-plt.axis('off')
-plt.title('closed 5 times')
-plt.imshow(closed, cmap='gray')
-plt.tight_layout()
-plt.show()
+    if key == ord('d'):
+        dilated_iteration = int(input('dilated_iteration: '))
+        dilated = cv2.morphologyEx(binary, cv2.MORPH_DILATE, (3,3), iterations=dilated_iteration)
+        #cv2.imshow('dilated / iteration : {0}'.format(dilated_iteration), dilated)
+        plt.imshow(dilated, cmap='gray')
+        plt.show()
+
+    if key == ord('o'):
+        opened_iteration = int(input('open_iteration: '))
+        opened = cv2.morphologyEx(binary, cv2.MORPH_OPEN,
+                                  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)),
+                                  iterations=opened_iteration)
+        #cv2.imshow('opened / iteration : {0}'.format(opened_iteration), opened)
+        plt.imshow(opened, cmap='gray')
+        plt.show()
+
+    if key == ord('c'):
+        closed_iteration = int(input('closed_iteration: '))
+        closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE,
+                                  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)),
+                                  iterations=closed_iteration)
+        #cv2.imshow('closed / iteration : {0}'.format(closed_iteration), closed)
+        plt.imshow(closed, cmap='gray')
+        plt.show()
+
+    if key == 27:
+        print("ESC Pressed")
+        break
+
+cv2.destroyAllWindows()
+
+
+
+
+
+
