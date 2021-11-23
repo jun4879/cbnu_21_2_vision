57d16200919f549fa3df3467f20dfb6de0dbfca1
KIM JUN TAE <57665924+jun4879@users.noreply.github.com>
2021-11-23 18:17:31 +0900

    hw3 작성

---
+++ 과제2/hw3_Panorama.py
@@ -0,0 +1,69 @@
+# 3. Panorama
+# CreaterStitcher 함수를 이용하여 4개의 영상 셋에 대해서 파노라마 이미지를 만드는 방법을 구현하시오.
+
+import cv2
+
+boat_images = []
+boat_images.append(cv2.imread('./data/boat1.jpg', cv2.IMREAD_COLOR))
+boat_images.append(cv2.imread('./data/boat2.jpg', cv2.IMREAD_COLOR))
+boat_images.append(cv2.imread('./data/boat3.jpg', cv2.IMREAD_COLOR))
+boat_images.append(cv2.imread('./data/boat4.jpg', cv2.IMREAD_COLOR))
+boat_images.append(cv2.imread('./data/boat5.jpg', cv2.IMREAD_COLOR))
+boat_images.append(cv2.imread('./data/boat6.jpg', cv2.IMREAD_COLOR))
+
+budapest_images = []
+budapest_images.append(cv2.imread('./data/budapest1.jpg', cv2.IMREAD_COLOR))
+budapest_images.append(cv2.imread('./data/budapest2.jpg', cv2.IMREAD_COLOR))
+budapest_images.append(cv2.imread('./data/budapest3.jpg', cv2.IMREAD_COLOR))
+budapest_images.append(cv2.imread('./data/budapest4.jpg', cv2.IMREAD_COLOR))
+budapest_images.append(cv2.imread('./data/budapest5.jpg', cv2.IMREAD_COLOR))
+budapest_images.append(cv2.imread('./data/budapest6.jpg', cv2.IMREAD_COLOR))
+
+newspaper_images = []
+newspaper_images.append(cv2.imread('./data/newspaper1.jpg', cv2.IMREAD_COLOR))
+newspaper_images.append(cv2.imread('./data/newspaper2.jpg', cv2.IMREAD_COLOR))
+newspaper_images.append(cv2.imread('./data/newspaper3.jpg', cv2.IMREAD_COLOR))
+newspaper_images.append(cv2.imread('./data/newspaper4.jpg', cv2.IMREAD_COLOR))
+
+s_images = []
+s_images.append(cv2.imread('./data/s1.jpg', cv2.IMREAD_COLOR))
+s_images.append(cv2.imread('./data/s2.jpg', cv2.IMREAD_COLOR))
+
+stitcher = cv2.Stitcher()
+
+boat_ret, boat_pano = stitcher.stitch(boat_images)
+budapest_ret, budapest_pano = stitcher.stitch(budapest_images)
+newspaper_ret, newspaper_pano = stitcher.stitch(newspaper_images)
+s_ret, s_pano = stitcher.stitch(s_images)
+
+if boat_ret == cv2.STITCHER_OK:
+    boat_pano_res = cv2.resize(boat_pano, dsize=(0, 0), fx=0.2, fy=0.2)
+    cv2.imshow('boat panorama', boat_pano_res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("boat pano Error")
+
+if budapest_ret == cv2.STITCHER_OK:
+    budapest_pano_res = cv2.resize(budapest_pano, dsize=(0, 0), fx=0.2, fy=0.2)
+    cv2.imshow('budapest panorama', budapest_pano_res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("budapest pano Error")
+
+if newspaper_ret == cv2.STITCHER_OK:
+    newspaper_pano_res = cv2.resize(newspaper_pano, dsize=(0, 0), fx=0.2, fy=0.2)
+    cv2.imshow('budapest panorama', newspaper_pano_res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("newspaper pano Error")
+
+if s_ret == cv2.STITCHER_OK:
+    s_pano_res = cv2.resize(s_pano, dsize=(0, 0), fx=0.2, fy=0.2)
+    cv2.imshow('budapest panorama', s_pano_res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("s pano Error")
---
+++ 과제2/hw4_OpticalFlow.py
@@ -0,0 +1,4 @@
+# dog 영상을 이용해서 Good Feature to Tracking을 추출하고 Pyramid Lucas-Kanade 알고리즘을 적용해서 Optical Flow를 구하는 코드를 작성하시오.
+# dog 영상을 이용해서 Farneback과 DualTVL1 Optical Flow 알고리즘을 구하는 코드를 작성하시오.
+
+import cv2
